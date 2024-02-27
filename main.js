683a93ec14038cbf82c5c185fede6be4d462036e
JoudHijaz <146662746+JoudHijaz@users.noreply.github.com>
2024-02-28 00:37:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    document.querySelector(".sidebar").style.width = "250px";
+    document.getElementById("items").style.width = "100%";
+}
+
+function closeSidebar() {
+    document.querySelector(".sidebar").style.width = "0";
+    document.getElementById("items").style.width = "0";
+}
